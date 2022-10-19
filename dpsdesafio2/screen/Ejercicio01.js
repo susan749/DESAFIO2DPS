5fe2730a617321fca8ccc4c2e98de0e95a5e5c14
portan17 <85976855+portan17@users.noreply.github.com>
2022-10-19 11:40:32 -0600

    primer ejercicio

--- dpsdesafio2/screen/Ejercicio01.js
+++ dpsdesafio2/screen/Ejercicio01.js
@@ -1,9 +1,41 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+// import React from 'react';
+// import { View, Text } from 'react-native';
+// export default function Ejercicio01() {
+//     return (
+//         <View>
+//             <Text> 1 </Text>
+//         </View>
+//     );
+// }
+
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, Switch, Text } from 'react-native';
+import { myColors } from '../src/styles/Colors';
+import { ThemeContext } from '../src/context/ThemeContext';
+import MyKeyboard from '../src/components/MyKeyboard';
+
+
+
 export default function Ejercicio01() {
-    return (
-        <View>
-            <Text> 1 </Text>
-        </View>
-    );
+  const [theme, setTheme] = useState('light');
+  return (
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView style={theme === 'light' ? styles.container : [styles.container, {backgroundColor: 'black'}]}>
+        <Switch
+          value={theme === 'dark'}
+          onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        />
+        <MyKeyboard />
+      </SafeAreaView>
+    </ThemeContext.Provider>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: myColors.light,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+});
